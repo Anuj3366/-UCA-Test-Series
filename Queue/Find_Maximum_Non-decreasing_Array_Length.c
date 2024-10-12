d0bf6d14a1a37c5c3f8ff57bb9455bcd4b43f8d7
Anuj3366 <anujgarg3366@gmail.com>
2024-10-12 22:00:12 +0530

    updated
    updated

--- Queue/Find_Maximum_Non-decreasing_Array_Length.c
+++ Queue/Find_Maximum_Non-decreasing_Array_Length.c
@@ -3,12 +3,19 @@ int findMaximumLength(int *nums, int numsSize)
   int ans = 1;
   int front = 0;
   int rear = 1;
+
   while (rear < numsSize)
   {
     if (nums[front] <= nums[rear])
     {
       front = rear;
       rear++;
+    }
+    else if (rear + 1 < numsSize && nums[front] > nums[rear] && (nums[front] + nums[rear]) <= nums[rear + 1])
+    {
+      nums[front] += nums[rear];
+      rear++;
+      ans--;
     }
     else
     {
